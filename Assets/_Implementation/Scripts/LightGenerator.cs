d1281c0cbd262dfc16fa796bdf2c0ab4b61e1fd1
sadri2020 <sadri.2020@yahoo.com>
2018-12-21 10:47:06 +0330

    Debug line renderer start position issue.

--- Assets/_Implementation/Scripts/LightGenerator.cs
+++ Assets/_Implementation/Scripts/LightGenerator.cs
@@ -6,69 +6,71 @@ using UnityEditor;
 [RequireComponent(typeof(LineRenderer))]
 public class LightGenerator : MonoBehaviour
 {
-	public Color color;
-	public Degree degree;
-	public LineRenderer _lineRenderer;
-	
-	public int maxReflectionCount = 5;
-	public float maxStepDistance = 200;
-	
-	public enum Degree
-	{
-		_0,
-		_45,
-		_90
-	}
-	
-	// Use this for initialization
-	void Start ()
-	{
-		_lineRenderer.startWidth = 0.45f;
-		_lineRenderer.endWidth = 0.45f;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		
-	}
-	
-	protected void OnDrawGizmos()
-	{
-		_lineRenderer.positionCount = maxReflectionCount;
-		
-		//_lineRenderer.startColor = color;
-		//_lineRenderer.endColor = color;
-		
-		//float alpha = 1.0f;
-		//Gradient gradient = new Gradient();
-		//gradient.SetKeys(
-		//	new GradientColorKey[] { new GradientColorKey(Color.green, 0.0f), new GradientColorKey(Color.red, 1.0f), new GradientColorKey(Color.red, 1.0f) },
+    public Degree degree;
+    public LineRenderer _lineRenderer;
 
-		//new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f), new GradientAlphaKey(alpha, 1.0f)  }
+    public int maxReflectionCount = 5;
+    public float maxStepDistance = 200;
 
-		//);
-		//_lineRenderer.colorGradient = gradient;
-		
-		Handles.color = Color.red;
-		Handles.ArrowHandleCap(0, this.transform.position + this.transform.forward * 0.25f, this.transform.rotation, 0.5f, EventType.Repaint);
-		Gizmos.color = Color.red;
-		Gizmos.DrawWireSphere(this.transform.position, 0.25f);
+    public enum Degree
+    {
+        _0,
+        _45,
+        _90
+    }
 
-		DrawPredictedReflectionPattern(this.transform.position + this.transform.forward * 0.75f, this.transform.forward, maxReflectionCount);
-	}
-	
-	private void DrawPredictedReflectionPattern(Vector3 position, Vector3 direction, int reflectionsRemaining)
-	{
-		if (reflectionsRemaining == 0) {
-			return;
-		}
+    // Use this for initialization
+    void Start()
+    {
+        _lineRenderer.startWidth = 0.45f;
+        _lineRenderer.endWidth = 0.45f;
+    }
 
-		Ray ray = new Ray(position, direction);
-		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit, maxStepDistance))
-		{
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    protected void OnDrawGizmos()
+    {
+        _lineRenderer.positionCount = maxReflectionCount;
+
+        //_lineRenderer.startColor = color;
+        //_lineRenderer.endColor = color;
+
+        //float alpha = 1.0f;
+        //Gradient gradient = new Gradient();
+        //gradient.SetKeys(
+        //	new GradientColorKey[] { new GradientColorKey(Color.green, 0.0f), new GradientColorKey(Color.red, 1.0f), new GradientColorKey(Color.red, 1.0f) },
+
+        //new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f), new GradientAlphaKey(alpha, 1.0f)  }
+
+        //);
+        //_lineRenderer.colorGradient = gradient;
+
+        Handles.color = Color.red;
+        Handles.ArrowHandleCap(0, this.transform.position + this.transform.forward * 0.25f, this.transform.rotation,
+            0.5f, EventType.Repaint);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, 0.25f);
+
+        _lineRenderer.SetPosition(0, this.transform.position);
+
+        DrawPredictedReflectionPattern(this.transform.position  + this.transform.forward * 0.75f,
+            this.transform.forward, maxReflectionCount-1);
+    }
+
+    private void DrawPredictedReflectionPattern(Vector3 position, Vector3 direction, int reflectionsRemaining)
+    {
+        if (reflectionsRemaining == 0)
+        {
+            return;
+        }
+
+        Ray ray = new Ray(position, direction);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, maxStepDistance))
+        {
             if (hit.collider.gameObject.CompareTag("Flat_mirror"))
             {
                 direction = Vector3.Reflect(direction, hit.normal);
@@ -76,8 +78,8 @@ public class LightGenerator : MonoBehaviour
             }
             else if (hit.collider.gameObject.CompareTag("Block"))
             {
-	            DrawAllRemainedPositions(hit.point, reflectionsRemaining);
-	            return;
+                DrawAllRemainedPositions(hit.point, reflectionsRemaining);
+                return;
             }
             else if (hit.collider.gameObject.CompareTag("Soorakh"))
             {
@@ -85,7 +87,9 @@ public class LightGenerator : MonoBehaviour
             }
             else if (hit.collider.gameObject.CompareTag("Prism"))
             {
-	            hit.collider.GetComponent<Pyramid>().ActivePyramid();
+                hit.collider.GetComponent<Pyramid>().ActivePyramid();
+                DrawAllRemainedPositions(hit.point, reflectionsRemaining);
+                return;
                 //LightGeneratorManager.instance.SetNextLightActive(hit.point);
             }
         }
@@ -95,19 +99,19 @@ public class LightGenerator : MonoBehaviour
         }
 
         Gizmos.color = Color.yellow;
-		//Gizmos.DrawLine(startingPosition, position);
-		_lineRenderer.SetPosition(maxReflectionCount - reflectionsRemaining, position);
+        //Gizmos.DrawLine(startingPosition, position);
+        _lineRenderer.SetPosition(maxReflectionCount - reflectionsRemaining, position);
 
-		    DrawPredictedReflectionPattern(position, direction, reflectionsRemaining - 1);
-	}
+        DrawPredictedReflectionPattern(position, direction, reflectionsRemaining - 1);
+    }
 
-	protected void DrawAllRemainedPositions(Vector3 pos, int reflectionsRemaining)
-	{
-		if (reflectionsRemaining == 0)
-			return;
-		
-		_lineRenderer.SetPosition(maxReflectionCount - reflectionsRemaining, pos);
+    protected void DrawAllRemainedPositions(Vector3 pos, int reflectionsRemaining)
+    {
+        if (reflectionsRemaining == 0)
+            return;
 
-		DrawAllRemainedPositions(pos, reflectionsRemaining - 1);
-	}
+        _lineRenderer.SetPosition(maxReflectionCount - reflectionsRemaining, pos);
+
+        DrawAllRemainedPositions(pos, reflectionsRemaining - 1);
+    }
 }
